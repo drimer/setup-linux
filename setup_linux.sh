78dcfd8390f031a3d5e6fc943fd00200089bc1e2
Alberto Aguilera <drimer.aaj@gmail.com>
2014-09-30 08:48:28 +0000

    setup_linux.sh: Move manual steps to text file
    It will make it easier to differenciate different sections in the next
    commit.

--- setup_linux.sh
+++ setup_linux.sh
@@ -74,13 +74,7 @@ ensure_puppet_installed() {
 }
 
 print_manual_setup() {
-    echo "Things that need to be manually customised by you:"
-    echo "1) Add, at least, one window to ~/.tmux.conf"
-    echo "2) Add pictures to wallpapoz"
-    echo "3) Log in with your user on Chromium to synchronize bookmarks and/or other things."
-    echo "4) Make sure you have the correct permissions in the directories ~/Downloads/rtorrent_*"
-    echo "And if you wish:"
-    echo "5) Remove ~/install_wallpapoz"
+    cat manual_steps.txt
 }
 
 main() {
