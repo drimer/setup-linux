ee23aabc50a4f4554ce766db7f9326382f44d2fb
Alberto Aguilera <drimer.aaj@gmail.com>
2016-11-07 07:04:21 +0000

    Add gnome an utils to Ansbile setup

---
+++ ansible/roles/gnome/files/install_minidlna.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+add-apt-repository -y ppa:stedy6/stedy-minidna
+apt-get update
+apt-get install -y minidlna
---
+++ ansible/roles/utils/files/install_jdownloader.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget http://installer.jdownloader.org/jd_unix_0_9.sh
+chmod +x jd_unix_*.sh
+./jd_unix_*.sh
+rm ./jd_unix_*.sh
